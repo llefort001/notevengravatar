cc35cee2c1376bab58cba710a9f6c846ca148e23
Adrien2602 <me@adriendemeyer.com>
2018-04-05 15:33:31 +0200

    fix

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
 
    <h1>Documentation</h1>
-   <script src="https://gist.github.com/AdrienDeMeyer/02d759f14745743d44498   47da6f36c34.js"></script>
+   <script src="https://gist.github.com/AdrienDeMeyer/02d759f14745743d4449847da6f36c34.js"></script>
 
 </div>
 
