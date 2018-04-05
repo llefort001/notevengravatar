7dcbde93ddb39275a86a2a98c91cfcd922754214
Lucas Lefort <negames64300@gmail.com>
2018-04-05 14:50:32 +0200

    minor fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <div class="row justify-content-center mt-5">
         <h1 class="display-1 text-center home-title">Not Even Gravatar</h1>
-        <div class="col-sm-6 text-center">
+        <div class="col-sm-12 text-center">
             <p>An "avatar" is an image that represents you online—a little picture that appears next to your name when you interact with websites.</p>
-            <p>A Gravatar is a Globally Recognized Avatar. You upload it and create your profile just once, and then when you participate in any NotEvenGravatar-enabled site, your Gravatar image will automatically follow you there.</p>
-            <p>NotEvenGravatar is a free service for site owners, developers, and users</p>
-            <p>Gravatar is run and supported by Lucas Lefort, Paul Laborde et Adrien De Meyer</p>
+            <p>A Not Even Gravatar is a Globally Recognized Avatar. You upload it and create your profile just once, and then when you participate in any Not Even Gravatar-enabled site, your Not Even Gravatar image will automatically follow you there.</p>
+            <p>Not Even Gravatar is a free service for site owners, developers, and users.</p>
+            <p>Not Even Gravatar is run and supported by Lucas Lefort, Paul Laborde et Adrien De Meyer.</p>
         </div>
     </div>
 
